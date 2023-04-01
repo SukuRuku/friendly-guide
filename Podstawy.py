bba8753be2fcbf8d14705b42d0bd63748c93830b
HaziksX <55769998+HaziksX@users.noreply.github.com>
2023-04-02 00:24:37 +0200

    pętla na sperme

--- Podstawy.py
+++ Podstawy.py
@@ -1,11 +1,10 @@
-sperma = input('Wpisz poziom spermy\n')
-sperma = int(sperma)
+while True:
+	sperma = input('Wpisz poziom spermy\n')
+	sperma = int(sperma)
 
-if sperma < 1:
-	print('zwal se konia')
-if sperma >= 1:
-	print('sperma')
-if sperma > 9:
-	print('zaraz dojde do spermy')
-
-input()
+	if sperma < 1:
+		print('zwal se konia')
+	if sperma >= 1:
+		print('sperma')
+	if sperma > 9:
+		print('zaraz dojde do spermy')
