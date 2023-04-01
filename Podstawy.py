87e18aedc051cdfc42d1361494996c84093b47e5
SukuRuku <129607999+SukuRuku@users.noreply.github.com>
2023-04-02 00:16:03 +0200

    Add files via upload
    To Polich people who learning Python try to cum.

---
+++ Podstawy.py
@@ -0,0 +1,11 @@
+sperma = input('Wpisz poziom spermy\n')
+sperma = int(sperma)
+
+if sperma < 1:
+	print('zwal se konia')
+if sperma >= 1:
+	print('sperma')
+if sperma > 9:
+	print('zaraz dojde do spermy')
+
+input()
